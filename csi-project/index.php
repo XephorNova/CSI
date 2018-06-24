65142269dd046f6c50a5c574879cfefa6b90693b
Idrees Dargahwala <idrees@theiyd.com>
2018-06-24 18:55:43 +0530

    DO NOT DISTURB THESE FILES. JUST FOR REFERENCE

---
+++ csi-project/index.php
@@ -0,0 +1,6 @@
+<head>
+    <title>Home - CSI</title>
+</head>
+<?php include('layout/header.php') ?>
+<h1 class="text-center">This is the homepage for CSI</h1>
+<?php include('layout/footer.php') ?>
---
+++ csi-project/pages/blog.php
@@ -0,0 +1,12 @@
+<head>
+    <title>Blog - CSI</title>
+</head>
+<?php include('../layout/header.php') ?>
+
+    <div class="container mt-4">
+        <h2 class="text-align">Blog</h2>
+
+        <div class="wrap-blog"></div>
+    </div>
+
+<?php include('../layout/footer.php') ?>
