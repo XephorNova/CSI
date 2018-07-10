d788d5621220abad6aaf09e936892d39e015dcab
XephorNova <35536922+XephorNova@users.noreply.github.com>
2018-07-11 00:48:31 +0530

    buttons added
    Added buttons to view existing problems and students data

--- admin/admin.php
+++ admin/admin.php
@@ -8,10 +8,13 @@
         echo '<div class="container mt-4 text-center">
         <h2>CSI Admin</h2>
         <p>A panel for CSI members</p>
-        <a href="./create_a_post.php"><button class="btn btn-dark">Create a post</button></a>
-        <a href="./view_posts.php"><button class="btn btn-dark">View Posts</button></a>
-        <a href="./create_a_problem.php"><button class="btn btn-dark">Create a problem</button></a>
-        <a href="../scripts/logout.php"><button class="btn btn-default">Logout</button></a>
+        <a href="./create_a_post.php"><button class="btn btn-dark  btn-md center-block">Create a post</button></a>
+        <a href="./view_posts.php"><button class="btn btn-dark  btn-md center-block">View Posts</button></a>
+        <a href="./create_a_problem.php"><button class="btn btn-dark  btn-md center-block">Create a problem</button></a>
+        <a href="./view_problems.php"><button class="btn btn-dark  btn-md center-block">View Problems</button></a>
+         <a href="./students.php"><button class="btn btn-dark  btn-md center-block">Students score</button></a>
+         <a href="#"><button class="btn btn-dark  btn-md center-block">Seminar notification</button></a>
+        <a href="../scripts/logout.php"><button class="btn btn-default  btn-md center-block">Logout</button></a>
     </div>';
     } else {
         header('Location: ./');
